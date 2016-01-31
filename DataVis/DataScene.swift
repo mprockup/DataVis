14d4a86f792bfb1fe5742813ea12fc63af59ff31
mprockup <mprockup@gmail.com>
2016-01-31 14:57:35 -0500

    plotting colors and new example

--- DataVis/DataScene.swift
+++ DataVis/DataScene.swift
@@ -18,35 +18,35 @@ class DataScene: SCNScene {
     
     override init() {
         super.init()
-        let sphereGeometry = SCNSphere(radius: 0.1)
-        let sphereNode = SCNNode(geometry: sphereGeometry)
-        self.rootNode.addChildNode(sphereNode)
-        
-        let secondSphereGeometry = SCNSphere(radius: 0.5)
-        let secondSphereNode = SCNNode(geometry: secondSphereGeometry)
-        secondSphereNode.position = SCNVector3(x: 40.0, y: 0.0, z: 0.0)
-        self.rootNode.addChildNode(secondSphereNode)
     }
     
     init(data:DataArray){
         super.init()
         plotData = data
+        var flatColors:[UIColor] = FlatUIColor.getDifferentColors()
         for d:Example in plotData.examples{
             let sphereGeometry = SCNSphere(radius: 0.1)
             let sphereNode = SCNNode(geometry: sphereGeometry)
             sphereNode.position = SCNVector3(x:d.x, y: d.y, z: d.z)
-            self.rootNode.addChildNode(sphereNode)
+            if plotData.labelType == LabelType.Categorical{
+                if d.label>=0{
+                    sphereGeometry.firstMaterial?.diffuse.contents = flatColors[Int(d.label)]
+                }
+            }
+            if d.label>=0{
+                self.rootNode.addChildNode(sphereNode)
+            }
         }
+    }
+    
+    func setPlotData(d:DataArray){
+        plotData = d
     }
     
     func setCameraPostion(pos:SCNVector3)
     {
 //        rootNode.position = pos
 //        cameraNode.position = pos
-    }
-    
-    func resetCameraPostion(){
-//        rootNode.position = SCNVector3(x: 0, y: 0, z: 0)
     }
     
     required init(coder aDecoder: NSCoder) {
--- DataVis/DataVis/DataArray.swift
+++ DataVis/DataVis/DataArray.swift
@@ -16,7 +16,7 @@ public enum LabelType {
 public class DataArray {
     
     public var examples: [Example] = []
-    var labelType: LabelType
+    public var labelType: LabelType
     var labelName: String!
     var xName: String!
     var yName: String!
@@ -94,6 +94,6 @@ public class Example {
         self.z = z;
         self.label = label;
         
-        print("Example with (x, y, z, label) = (", x, ", ", y, ", ", z, ", ", label, ")");
+//        print("Example with (x, y, z, label) = (", x, ", ", y, ", ", z, ", ", label, ")");
     }
 }
